c3c9692fa9399c350a1c0a4a12e78accbd439410
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-01 17:04:31 -0600

    feat(Controllers): Agregamos el endpoint "/createTask"
    A traves de este endpoint de tipo POST podemos crear nuevas tareas en nuestra base de datos

--- src/main/java/com/example/rest/Controllers/ToDoController.java
+++ src/main/java/com/example/rest/Controllers/ToDoController.java
@@ -4,6 +4,8 @@ import com.example.rest.Models.Task;
 import com.example.rest.Repositorys.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,12 +16,18 @@ public class ToDoController {
     private TaskRepository taskRepository;
 
     @GetMapping (value = "/")
-    public String holaMundo () {
-        return "Hola Mundo!";
+    public String welcome () {
+        return "Welcome to ToDo App!";
     }
 
     @GetMapping (value = "/tasks")
     public List <Task> getTasks () {
         return taskRepository.findAll();
+    }
+
+    @PostMapping (value = "/createTask")
+    public String postTask (@RequestBody Task task) {
+        taskRepository.save(task);
+        return "Task Created";
     }
 }
