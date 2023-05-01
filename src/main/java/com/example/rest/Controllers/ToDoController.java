9a22652eb2aa6c96c0d0f472fadd8d22baeeda45
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-01 16:18:47 -0600

    feat(Controllers): Agregamos el endpoint "/tasks"
    A traves de este endpoint es como vamos a poder recuperar todas las tareas que tengamos persistentes en base de datos

--- src/main/java/com/example/rest/Controllers/ToDoController.java
+++ src/main/java/com/example/rest/Controllers/ToDoController.java
@@ -1,13 +1,25 @@
 package com.example.rest.Controllers;
 
+import com.example.rest.Models.Task;
+import com.example.rest.Repositorys.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ToDoController {
+    @Autowired
+    private TaskRepository taskRepository;
 
-    @GetMapping(value = "/")
+    @GetMapping (value = "/")
     public String holaMundo () {
         return "Hola Mundo!";
+    }
+
+    @GetMapping (value = "/tasks")
+    public List <Task> getTasks () {
+        return taskRepository.findAll();
     }
 }
