4c4ae0bf9b0b4eec031d36196f577d177caf589e
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-01 15:48:02 -0600

    feat(Models): Agregamos las etiquetas de JPA
    Agregamos todas las etiquetas necesarias de JPA para que nuestra clase pueda ser mapeada

--- src/main/java/com/example/rest/Models/Task.java
+++ src/main/java/com/example/rest/Models/Task.java
@@ -1,9 +1,25 @@
 package com.example.rest.Models;
 
+import jakarta.persistence.*;
+
 @Entity
+@Table (name = "task")
 public class Task {
+    @Id
+    @SequenceGenerator (
+            name = "task_sequence",
+            sequenceName = "task_sequence",
+            allocationSize = 1,
+            initialValue = 1
+    )
+    @GeneratedValue (
+            strategy = GenerationType.SEQUENCE,
+            generator = "task_sequence"
+    )
     private Long id;
+    @Column (name = "title")
     private String title;
+    @Column (name = "description")
     private String description;
 
     public Task(Long id, String title, String description) {
